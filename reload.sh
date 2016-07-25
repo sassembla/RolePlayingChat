4b90fdfbcc863dea6230ca4ea9a1c27335a77ef9
Toru <toru.inoue@kissaki.tv>
2016-07-26 01:02:28 +0900

    updating nginx

--- reload.sh
+++ reload.sh
@@ -1,8 +1,13 @@
+# このへんの機能を、CrossPeerとして実装する。
+
+# 対象のフォルダを見つけてくる機能
 cp -Rp ./Assets/XrossPeer/Disquuun ./CoreCLR/
 
+# このへんは蛇足だな〜
 ps aux | grep [d]isque-server | awk '{print $2}' | xargs sudo kill -9
 nohup ./Server/disque/src/disque-server > /dev/null 2>&1 &
 
+# 実行 ここまではやろう。
 cd CoreCLR
 dotnet run
 cd ../
